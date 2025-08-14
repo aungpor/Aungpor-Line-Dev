17ee97cffe98443ac5f037b3d37cea2ced5abeb5
Naphat <Naphat.pa@pt.co.th>
2025-08-14 14:41:49 +0700

    firebase init

---
+++ lib/auth.ts
@@ -0,0 +1,19 @@
+// lib/auth.ts
+import { useEffect, useState } from "react";
+import { auth } from "./firebase"; // import จาก firebase.ts
+import { onAuthStateChanged, User } from "firebase/auth";
+
+export function useAuth() {
+  const [user, setUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setLoading(false);
+    });
+    return () => unsubscribe();
+  }, []);
+
+  return { user, loading };
+}
